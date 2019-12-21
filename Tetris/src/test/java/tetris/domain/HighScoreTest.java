3b8100e84aa2f5ed6dbc7b5fd5bdf2536d14dbf5
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2019-12-21 22:26:03 +0200

    Testailua

--- Tetris/src/test/java/tetris/domain/HighScoreTest.java
+++ Tetris/src/test/java/tetris/domain/HighScoreTest.java
@@ -51,7 +51,6 @@ public class HighScoreTest {
 
     @Test
     public void testHighScoresDatFile() throws IOException {
-        //Path myTempFolder = Files.createTempDirectory(null);
         Path path2 = null;
         File copy = null;
         if (new File("HighScores.dat").exists()) {
@@ -62,28 +61,37 @@ public class HighScoreTest {
             copy = path2.toFile();
             original.delete();
         }
-        //File targetFile = new File(myTempFolder, original.getName());
-        //File tempFile = tempFolder.newFile("HighScores.dat");
-        //if (path2 != null && new File("HighScore3.dat").exists()) {
-            HighScore[] testList = HighScore.getHighScores();
+        HighScore[] testList = HighScore.getHighScores();
         for (int i = 0; i < testList.length; i++) {
             assertEquals(highScores[i].toString(), testList[i].toString());
-        //}
         }
-        //File testFile = new File(path.toString());
-        //testFile.delete();
         if (copy != null) {
             Files.move(path2, path2.resolveSibling("HighScores.dat"), REPLACE_EXISTING);
-            //File copy = new File(FileSystems.getDefault().toString() + "HighScores3.dat");
             copy.delete();
         }
     }
 
-    /*@Test
+    @Test
     public void addHighScore() throws IOException {
+        Path path2 = null;
+        File copy = null;
+        if (new File("HighScores.dat").exists()) {
+            Path path = Paths.get("HighScores.dat");
+            File original = path.toFile();
+            Files.move(path, path.resolveSibling("HighScores3.dat"));
+            path2 = Paths.get("HighScores3.dat");
+            copy = path2.toFile();
+            original.delete();
+        }
         HighScore.addHighScore(highScore);
-        
-    }*/
+        HighScore[] testList = HighScore.getHighScores();
+        assertEquals(highScore.toString(), testList[0].toString());
+        if (copy != null) {
+            Files.move(path2, path2.resolveSibling("HighScores.dat"), REPLACE_EXISTING);
+            copy.delete();
+        }
+    }
+    
     @Test
     public void hiscoreHeader() {
         assertEquals("TOP 10\n\n           NIMI  PISTEET  TASO  RIVIT \n\n",

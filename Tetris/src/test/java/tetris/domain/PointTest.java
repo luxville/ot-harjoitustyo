911d3a41f93319970b8f5bc7043c3711e713b3d4
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2019-12-21 13:12:27 +0200

    Testailua

--- Tetris/src/test/java/tetris/domain/PointTest.java
+++ Tetris/src/test/java/tetris/domain/PointTest.java
@@ -8,60 +8,50 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PointTest {
-
-    public PointTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+Point point;
+Point typedPoint;
 
     @Before
     public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
+        point = new Point(1, 1);
+        typedPoint = new Point(1, 1, 2);
     }
 
     @Test
-    public void create_point() {
-        Point point = new Point(1, 1);
+    public void createPoint() {
         assertEquals(point.x, 1);
         assertEquals(point.y, 1);
     }
 
     @Test
-    public void create_typed_point() {
-        Point point = new Point(1, 1, 2);
-        assertEquals(point.x, 1);
-        assertEquals(point.y, 1);
-        assertEquals(point.type, 2);
+    public void createTypedPoint() {
+        assertEquals(typedPoint.x, 1);
+        assertEquals(typedPoint.y, 1);
+        assertEquals(typedPoint.type, 2);
     }
 
     @Test
-    public void mod_X() {
-        Point point = new Point(1, 1);
+    public void modX() {
         point.modX(2);
         assertEquals(point.x, 3);
         assertEquals(point.y, 1);
     }
     
     @Test
-    public void mod_Y() {
-        Point point = new Point(1, 1);
+    public void modY() {
         point.modY(2);
         assertEquals(point.x, 1);
         assertEquals(point.y, 3);
     }
     
     @Test
-    public void get_type() {
-        Point point = new Point(1, 1, 2);
-        assertEquals(2, point.getType());
+    public void getType() {
+        assertEquals(2, typedPoint.getType());
+    }
+    
+    @Test
+    public void notPoint() {
+        Shape shape = new Shape(7);
+        assertNotEquals(point, shape);
     }
 }

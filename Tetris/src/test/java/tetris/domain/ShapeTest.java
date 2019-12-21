cc073603389517ef29be5a66599b503005df4e59
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2019-12-21 11:43:53 +0200

    Testailua

--- Tetris/src/test/java/tetris/domain/ShapeTest.java
+++ Tetris/src/test/java/tetris/domain/ShapeTest.java
@@ -26,87 +26,72 @@ public class ShapeTest {
     List<Point> jPoints;
     List<Point> oPoints;
 
-    public ShapeTest() {
-    }
+    /*public ShapeTest() {
+    }*/
 
-    @BeforeClass
+    /*@BeforeClass
     public static void setUpClass() {
-    }
+    }*/
 
-    @AfterClass
+    /*@AfterClass
     public static void tearDownClass() {
-    }
+    }*/
 
     @Before
     public void setUp() {
-        Shape l = new Shape(1);
-        Shape i = new Shape(2);
-        Shape t = new Shape(3);
-        Shape s = new Shape(4);
-        Shape z = new Shape(5);
-        Shape j = new Shape(6);
-        Shape o = new Shape(7);
+        l = new Shape(1);
+        i = new Shape(2);
+        t = new Shape(3);
+        s = new Shape(4);
+        z = new Shape(5);
+        j = new Shape(6);
+        o = new Shape(7);
 
-        List<Point> lPoints = l.getPoints();
-        List<Point> iPoints = i.getPoints();
-        List<Point> tPoints = t.getPoints();
-        List<Point> sPoints = s.getPoints();
-        List<Point> zPoints = z.getPoints();
-        List<Point> jPoints = j.getPoints();
-        List<Point> oPoints = o.getPoints();
+        lPoints = l.getPoints();
+        iPoints = i.getPoints();
+        tPoints = t.getPoints();
+        sPoints = s.getPoints();
+        zPoints = z.getPoints();
+        jPoints = j.getPoints();
+        oPoints = o.getPoints();
     }
 
-    @After
+    /*@After
     public void tearDown() {
-    }
+    }*/
 
     @Test
     public void createPointsTest() {
-        /*Shape l = new Shape(1);
-        Shape i = new Shape(2);
-        Shape t = new Shape(3);
-        Shape s = new Shape(4);
-        Shape z = new Shape(5);
-        Shape j = new Shape(6);
-        Shape o = new Shape(7);
-         */
-        //List<Point> points = l.getPoints();
         assertThat(lPoints.get(0), equalTo(new Point(5, 0)));
         assertThat(lPoints.get(1), equalTo(new Point(4, 1)));
         assertThat(lPoints.get(2), equalTo(new Point(5, 1)));
         assertThat(lPoints.get(3), equalTo(new Point(3, 1)));
 
-        //points = i.getPoints();
         assertThat(iPoints.get(0), equalTo(new Point(3, 0)));
         assertThat(iPoints.get(1), equalTo(new Point(4, 0)));
         assertThat(iPoints.get(2), equalTo(new Point(5, 0)));
         assertThat(iPoints.get(3), equalTo(new Point(6, 0)));
 
-        //tPoints = t.getPoints();
         assertThat(tPoints.get(0), equalTo(new Point(4, 0)));
         assertThat(tPoints.get(1), equalTo(new Point(4, 1)));
         assertThat(tPoints.get(2), equalTo(new Point(5, 1)));
         assertThat(tPoints.get(3), equalTo(new Point(3, 1)));
 
-        // sPoints = s.getPoints();
         assertThat(sPoints.get(0), equalTo(new Point(4, 0)));
         assertThat(sPoints.get(1), equalTo(new Point(5, 0)));
         assertThat(sPoints.get(2), equalTo(new Point(4, 1)));
         assertThat(sPoints.get(3), equalTo(new Point(3, 1)));
 
-        //zPoints = z.getPoints();
         assertThat(zPoints.get(0), equalTo(new Point(3, 0)));
         assertThat(zPoints.get(1), equalTo(new Point(4, 0)));
         assertThat(zPoints.get(2), equalTo(new Point(4, 1)));
         assertThat(zPoints.get(3), equalTo(new Point(5, 1)));
 
-        // jPoints = j.getPoints();
         assertThat(jPoints.get(0), equalTo(new Point(3, 0)));
         assertThat(jPoints.get(1), equalTo(new Point(4, 1)));
         assertThat(jPoints.get(2), equalTo(new Point(5, 1)));
         assertThat(jPoints.get(3), equalTo(new Point(3, 1)));
 
-        // oPoints = o.getPoints();
         assertThat(oPoints.get(0), equalTo(new Point(4, 0)));
         assertThat(oPoints.get(1), equalTo(new Point(5, 0)));
         assertThat(oPoints.get(2), equalTo(new Point(4, 1)));
@@ -115,25 +100,19 @@ public class ShapeTest {
 
     @Test
     public void moveShapeLeft() {
-        //Shape l = new Shape(1);
-        //Shape i = new Shape(2);
-        //Shape t = new Shape(3);
 
-        //List<Point> points = l.getPoints();
         l.moveLeft();
         assertThat(lPoints.get(0), equalTo(new Point(4, 0)));
         assertThat(lPoints.get(1), equalTo(new Point(3, 1)));
         assertThat(lPoints.get(2), equalTo(new Point(4, 1)));
         assertThat(lPoints.get(3), equalTo(new Point(2, 1)));
 
-        //points = i.getPoints();
         i.moveLeft();
         assertThat(iPoints.get(0), equalTo(new Point(2, 0)));
         assertThat(iPoints.get(1), equalTo(new Point(3, 0)));
         assertThat(iPoints.get(2), equalTo(new Point(4, 0)));
         assertThat(iPoints.get(3), equalTo(new Point(5, 0)));
 
-        //points = t.getPoints();
         t.moveLeft();
         assertThat(tPoints.get(0), equalTo(new Point(3, 0)));
         assertThat(tPoints.get(1), equalTo(new Point(3, 1)));
@@ -144,18 +123,12 @@ public class ShapeTest {
     @Test
     public void moveShapeRight() {
 
-        //Shape s = new Shape(4);
-        //Shape z = new Shape(5);
-
-        //List<Point> points = s.getPoints();
-        //points = s.getPoints();
         s.moveRight();
         assertThat(sPoints.get(0), equalTo(new Point(5, 0)));
         assertThat(sPoints.get(1), equalTo(new Point(6, 0)));
         assertThat(sPoints.get(2), equalTo(new Point(5, 1)));
         assertThat(sPoints.get(3), equalTo(new Point(4, 1)));
 
-        //points = z.getPoints();
         z.moveRight();
         assertThat(zPoints.get(0), equalTo(new Point(4, 0)));
         assertThat(zPoints.get(1), equalTo(new Point(5, 0)));
@@ -165,37 +138,25 @@ public class ShapeTest {
 
     @Test
     public void moveShapeDown() {
-        Shape j = new Shape(6);
-        Shape o = new Shape(7);
-
-        List<Point> points = j.getPoints();
 
         j.moveDown();
-        assertThat(points.get(0), equalTo(new Point(3, 1)));
-        assertThat(points.get(1), equalTo(new Point(4, 2)));
-        assertThat(points.get(2), equalTo(new Point(5, 2)));
-        assertThat(points.get(3), equalTo(new Point(3, 2)));
+        assertThat(jPoints.get(0), equalTo(new Point(3, 1)));
+        assertThat(jPoints.get(1), equalTo(new Point(4, 2)));
+        assertThat(jPoints.get(2), equalTo(new Point(5, 2)));
+        assertThat(jPoints.get(3), equalTo(new Point(3, 2)));
 
-        points = o.getPoints();
         o.moveDown();
-        assertThat(points.get(0), equalTo(new Point(4, 1)));
-        assertThat(points.get(1), equalTo(new Point(5, 1)));
-        assertThat(points.get(2), equalTo(new Point(4, 2)));
-        assertThat(points.get(3), equalTo(new Point(5, 2)));
+        assertThat(oPoints.get(0), equalTo(new Point(4, 1)));
+        assertThat(oPoints.get(1), equalTo(new Point(5, 1)));
+        assertThat(oPoints.get(2), equalTo(new Point(4, 2)));
+        assertThat(oPoints.get(3), equalTo(new Point(5, 2)));
     }
 
     @Test
     public void getShapeType() {
-        Shape l = new Shape(1);
-        Shape i = new Shape(2);
-        Shape t = new Shape(3);
-        Shape s = new Shape(4);
-        Shape z = new Shape(5);
-        Shape j = new Shape(6);
-        Shape o = new Shape(7);
 
-        assertThat(l.getType(), equalTo(l.getType()));
-        assertThat(z.getType(), equalTo(z.getType()));
-        assertThat(o.getType(), equalTo(o.getType()));
+        assertThat(1, equalTo(l.getType()));
+        assertThat(5, equalTo(z.getType()));
+        assertThat(7, equalTo(o.getType()));
     }
 }
